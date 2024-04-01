5c32592ca4f5c0f997abbb1195595b69b5eac3f2
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2024-04-01 23:17:50 +0900

    modify detr/dataset.py

--- detr/dataset.py
+++ detr/dataset.py
@@ -36,23 +36,14 @@ class CocoDetection(torchvision.datasets.CocoDetection):
     '''
     def __getitem__(self, idx: int):
         img, target = super().__getitem__(idx)
-
-        # 親クラスのコンストラクタでself.idsに画像IDが
-        # 格納されているのでそれを取得
         img_id = self.ids[idx]
 
         # 物体の集合を一つの矩形でアノテーションしているものを除外
         target = [obj for obj in target if 'iscrowd' not in obj or obj['iscrowd'] == 0]
 
-        # 学習用に当該画像に映る物体のクラスIDと矩形を取得
-        # クラスIDはコンストラクタで新規に割り当てたIDに変換
-        classes = torch.tensor([self.coco_to_pred[obj['category_id']]
-                                for obj in target], dtype=torch.int64)
-        boxes = torch.tensor([obj['bbox'] for obj in target],
-                             dtype=torch.float32)
+        classes = torch.tensor([self.coco_to_pred[obj['category_id']] for obj in target], dtype=torch.int64)
+        boxes = torch.tensor([obj['bbox'] for obj in target], dtype=torch.float32)
 
-        # 矩形が0個のとき、boxes.shape == [0]となってしまうため、
-        # 第1軸に4を追加して軸数と第2軸の次元を合わせる
         if boxes.shape[0] == 0:
             boxes = torch.zeros((0, 4))
 
@@ -60,12 +51,9 @@ class CocoDetection(torchvision.datasets.CocoDetection):
         # xmin, ymin, width, height -> xmin, ymin, xmax, ymax
         boxes[:, 2:] += boxes[:, :2]
 
-        # 矩形が画像領域内に収まるように値をクリップ
         boxes[:, ::2] = boxes[:, ::2].clamp(min=0, max=width)
         boxes[:, 1::2] = boxes[:, 1::2].clamp(min=0, max=height)
 
-        # 学習のための正解データを用意
-        # クラスIDや矩形など渡すものが多義にわたるため、辞書で用意
         target = {
             'image_id': torch.tensor(img_id, dtype=torch.int64),
             'classes': classes,
@@ -75,7 +63,6 @@ class CocoDetection(torchvision.datasets.CocoDetection):
             'orig_img': torch.tensor(np.asarray(img))
         }
 
-        # データ拡張と整形
         if self.transform is not None:
             img, target = self.transform(img, target)
 
